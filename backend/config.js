931b42144e7f59ecc610e6a404de783ab0f0759d
kuba <cubax@mail.ru>
2019-01-17 14:20:41 +0700

    upd

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	mongoURI: "mongodb+srv://kubines:772ps4932@cluster0-dzpta.mongodb.net/test?retryWrites=true",
+	mongoURI: "mongodb+srv://kubines:freedb772@cluster0-dzpta.mongodb.net/test?retryWrites=true",
 }
